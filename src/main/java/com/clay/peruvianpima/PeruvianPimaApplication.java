736a58d8d71a0207636f966979fab578c3749e03
Clay Carroll <claycarroll7@gmail.com>
2023-09-15 11:06:42 -0400

    initial commit. Connected to database

---
+++ src/main/java/com/clay/peruvianpima/PeruvianPimaApplication.java
@@ -0,0 +1,13 @@
+package com.clay.peruvianpima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeruvianPimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeruvianPimaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clay/peruvianpima/PeruvianPimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clay.peruvianpima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeruvianPimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

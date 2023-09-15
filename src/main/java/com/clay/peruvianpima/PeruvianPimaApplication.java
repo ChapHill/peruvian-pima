39a5fa901be46d0c87906f6eeef6a1a2f221e6f3
Clay Carroll <claycarroll7@gmail.com>
2023-09-15 14:18:54 -0400

    having trouble figuring out automatic table creation... slowly going insane... nuking code and branching off of main

--- src/main/java/com/clay/peruvianpima/PeruvianPimaApplication.java
+++ src/main/java/com/clay/peruvianpima/PeruvianPimaApplication.java
@@ -1,7 +1,11 @@
 package com.clay.peruvianpima;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
 
 @SpringBootApplication
 public class PeruvianPimaApplication {
@@ -9,5 +13,7 @@ public class PeruvianPimaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PeruvianPimaApplication.class, args);
 	}
+
+
 
 }
